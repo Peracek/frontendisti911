03fe03d527210886c45390c426967b3123626a60
Pavel Peroutka <pavel.peroutka@ataccama.com>
2022-11-09 19:18:41 +0100

    more functional attempt

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ import academicTitles from './data/academicTitles.json'
 // DON'T FORGET TO USE GIT
 
 const isAcademic = (title) => academicTitles.includes(title)
+const titlesCompare = (c1, c2) => c2.titles.length - c1.titles.length
 
 const getSmartest = (candidates) => {
-  let smartest = candidates[0]
-
-  for (const candidate of candidates) {
-    if (candidate.titles.filter(isAcademic).length > smartest.titles.filter(isAcademic).length) {
-      smartest = candidate
-    }
-  }
-
+  const sorted = candidates
+    .map((cand) => ({ ...cand, titles: cand.titles.filter(isAcademic) }))
+    .sort(titlesCompare)
+  const smartest = sorted[0]
   return `${smartest.titles.join(' ')} ${smartest.name}`
 }
 
