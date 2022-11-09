57aebb67f81516adbc0ed9f454041858b1165938
Pavel Peroutka <pavel.peroutka@ataccama.com>
2022-11-09 19:23:26 +0100

    ramda attempt

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+import * as R from 'ramda'
 import candidates from './data/candidates.json'
 import academicTitles from './data/academicTitles.json'
 
 // DON'T FORGET TO USE GIT
 
 const isAcademic = (title) => academicTitles.includes(title)
-const titlesCompare = (c1, c2) => c2.titles.length - c1.titles.length
+const titlesCompare = R.descend(R.path(['titles', 'length']))
+const format = (candidate) => `${candidate.titles.join(' ')} ${candidate.name}`
 
-const getSmartest = (candidates) => {
-  const sorted = candidates
-    .map((cand) => ({ ...cand, titles: cand.titles.filter(isAcademic) }))
-    .sort(titlesCompare)
-  const smartest = sorted[0]
-  return `${smartest.titles.join(' ')} ${smartest.name}`
-}
+const getSmartest = R.pipe(
+  R.map((cand) => R.evolve({ titles: R.filter(isAcademic) }, cand)),
+  R.sort(titlesCompare),
+  R.head,
+  format
+)
 
 getSmartest(candidates) /*?*/
